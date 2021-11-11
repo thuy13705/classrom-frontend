8b9f946a85bca517ab543673ca28532de31e3b65
Anh Thư Phạm <thupham@Anhs-MacBook-Air.local>
2021-11-11 12:14:10 +0700

    create frontend for login-register

---
+++ src/component/login-register/Login.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './index.css';
+
+function Login() {
+    return (
+        <form>
+            <h2>Classroom</h2>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <h3>Login</h3>
+                    <div className="form-group">
+                        <label>Email address</label>
+                        <input type="email" className="form-control" placeholder="Enter email" />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" />
+                    </div>
+
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="btn btn-block">Login</button>
+
+                    <div className="form-group">
+                        <div>
+                            <label className="login-with">Login with</label>
+                        </div>
+                        <button class="loginBtn loginBtn--google">
+                            Google
+                        </button>
+                        <button class="loginBtn loginBtn--facebook">
+                            Facebook
+                        </button>
+                    </div>
+                    <p className="register">
+                        You haven't an account? <Link to="/register">Create an new account</Link>
+                    </p>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default Login
