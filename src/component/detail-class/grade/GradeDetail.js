881d3ebfdd56f194ccabb3a0e9d7ea1ea4c1dee5
ngocchi111 <18120010@student.hcmus.edu.vn>
2021-12-21 08:21:03 +0700

    upload csv

--- src/component/detail-class/grade/GradeDetail.js
+++ src/component/detail-class/grade/GradeDetail.js
@@ -5,6 +5,7 @@ import {
 import { CSVLink } from "react-csv";
 import { useHistory } from 'react-router-dom';
 import './../index.css'
+import ImportFile from './ImportFile';
 
 
 function GradeDetail({ items, setItems, getDetail }) {
@@ -13,6 +14,130 @@ function GradeDetail({ items, setItems, getDetail }) {
     const [headers, setHeader] = useState([]);
     const [data, setData] = useState([]);
     const [csvReport, setCSVReport] = useState([]);
+    const [fileStudentList, setFileStudentList] = useState();
+    const [fileGrade, setFileGrade] = useState();
+    const [onHide, setOnHide] = useState(false);
+    const [gradeDropDown, setGradeDropDown] = useState(false);
+    const [grade, setGrade] = useState();
+
+    const setStudentList = (input) =>{
+        setFileStudentList(input.files[0]);
+        var reader = new FileReader();
+        reader.readAsText(input.files[0]);
+
+        reader.onload = function() {
+            const strData = reader.result;
+            let arrData = [];
+            let lines = strData.split(/\r\n|\n|,/);
+            
+            let i=2;
+            while (i < lines.length)
+            {
+                while (lines[i] && lines[i]!="" && lines[i].indexOf('"') !=-1){
+                    lines[i] = lines[i].replace('"','');
+                }
+                while (lines[i+1] && lines[i+1]!="" &&  lines[i+1].indexOf('"') !=-1){
+                    lines[i+1] = lines[i+1].replace('"','');
+                }
+                let Data ={
+                    studentID: lines[i],
+                    name: lines[i+1],
+                }
+                if (lines[i] && lines[i+1])
+                arrData.push(Data);
+                i=i+2;
+            }
+            console.log(arrData);
+            const myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
+        myHeaders.append("Accept", "application/json");
+        myHeaders.append("Content-Type", "application/json");
+            fetch('http://localhost:3080/classes/boardGrade/' + items._id , {
+                method: 'POST',
+                headers: myHeaders,
+                body: JSON.stringify({
+                    datas: arrData
+                }),
+                mode:"cors",
+                
+            })
+                .then(async(message) => {
+                    if (message!=="success"){
+                       const result=await getDetail();
+                       setItems(result)
+                    }
+                    else{
+                       history.push('/signin')
+                    }
+                }, (error) => {
+                    alert(error);
+                })
+        }
+    }
+
+    const setPointList = (input) =>{
+        setFileStudentList(input.files[0]);
+        var reader = new FileReader();
+        reader.readAsText(input.files[0]);
+
+        reader.onload = function() {
+            const strData = reader.result;
+            let arrData = [];
+            let lines = strData.split(/\r\n|\n|,/);
+            
+            let i=2;
+            while (i < lines.length)
+            {
+                while (lines[i] && lines[i]!="" && lines[i].indexOf('"') !=-1){
+                    lines[i] = lines[i].replace('"','');
+                }
+                while (lines[i+1] && lines[i+1]!="" &&  lines[i+1].indexOf('"') !=-1){
+                    lines[i+1] = lines[i+1].replace('"','');
+                }
+                let Data ={
+                    studentID: lines[i],
+                    point: parseInt(lines[i+1]),
+                }
+                if (lines[i] && lines[i+1])
+                arrData.push(Data);
+                i=i+2;
+            }
+            console.log(arrData);
+            
+            const myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
+        myHeaders.append("Accept", "application/json");
+        myHeaders.append("Content-Type", "application/json");
+            fetch('http://localhost:3080/grade/pushAllPoint/' + grade._id , {
+                method: 'POST',
+                headers: myHeaders,
+                body: JSON.stringify({
+                    datas: arrData
+                }),
+                mode:"cors",
+                
+            })
+                .then(async(message) => {
+                    if (message!=="success"){
+                       const result=await getDetail();
+                       setItems(result)
+                    }
+                    else{
+                       history.push('/signin')
+                    }
+                }, (error) => {
+                    alert(error);
+                })
+        }
+    }
+
+    const setInput = (input) =>{
+        if (gradeDropDown)
+            setPointList(input)
+        else    
+            setStudentList(input);
+        setOnHide(!onHide);
+    }
 
     const handleData = () => {
 
@@ -55,7 +180,9 @@ function GradeDetail({ items, setItems, getDetail }) {
                         </Dropdown.Toggle>
 
                         <Dropdown.Menu>
-                            <Dropdown.Item>Action</Dropdown.Item>
+                            <Dropdown.Item >
+                                <div onClick={()=>{setOnHide(!onHide); setGradeDropDown(false)}}>Import student list</div>
+                            </Dropdown.Item>
                             <Dropdown.Item >Another action</Dropdown.Item>
                             <Dropdown.Item >Something else</Dropdown.Item>
                         </Dropdown.Menu>
@@ -68,7 +195,7 @@ function GradeDetail({ items, setItems, getDetail }) {
                         <Dropdown.Menu>
                             <Dropdown.Menu>
                                 <Dropdown.Item as="Label">
-                                    <CSVLink style={{ textDecoration: "none", color: "#272343" }} {...csvReport}>Export student list</CSVLink>
+                                    <CSVLink style={{ textDecoration: "none", color: "#272343" }} data={data} headers={headers} filename="StudentList.csv">Export student list</CSVLink>
                                 </Dropdown.Item>
                                 <Dropdown.Item as="Label">
                                     <CSVLink style={{ textDecoration: "none", color: "#272343" }}  {...csvReport}>Export template grade list</CSVLink>
@@ -81,6 +208,11 @@ function GradeDetail({ items, setItems, getDetail }) {
                         </Dropdown.Menu>
                     </Dropdown>
                 </div>
+                <ImportFile
+                                    show={onHide}
+                                    onHide={setOnHide}
+                                    setFile={setInput}
+                                />
                 <Table id="emp" bordered hover>
                     <thead>
                         <tr>
@@ -88,21 +220,35 @@ function GradeDetail({ items, setItems, getDetail }) {
                             <th style={{ width: "200px" }}>Name</th>
                             <th style={{ width: "100px" }}>Average</th>
                             {items.grades && items.grades.map((item, index) => (
-                                <th style={{ width: "100px" }} key={item._id}>{item.name}</th>
+                                <th style={{ width: "100px" }} key={item._id}> {item.name} 
+                                <Dropdown style={{display:"inline", marginLeft: "50px", marginRight:"0px"}}>
+                                <Dropdown.Toggle >
+                                    &#10247;
+                                </Dropdown.Toggle>
+        
+                                <Dropdown.Menu>
+                                    <Dropdown.Item >
+                                        <div onClick={()=>{setOnHide(!onHide); setGradeDropDown(true); setGrade(item)}}>Import student list</div>
+                                    </Dropdown.Item>
+                                    <Dropdown.Item >Another action</Dropdown.Item>
+                                    <Dropdown.Item >Something else</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown></th>
                             ))}
                         </tr>
                     </thead>
                     <tbody>
-                        {items.teachers && items.teachers.map((student, index) => (
-                            <tr key={student._id}>
+                        {items.boardGrade && items.boardGrade.map((student, index) => (
+                            <tr key={student.studentID}>
                                 <td style={{ width: "100px" }}>{student.studentID}</td>
                                 <td style={{ width: "200px" }}>{student.name}</td>
                                 <td contentEditable="true" style={{ width: "100px" }}>
-                                     
                                     </td>
-                                {items.grades && items.grades.map((grade, index) => (
-                                    <td contentEditable="true" style={{ width: "100px" }}>
-                                       
+                                {student.point && student.point.map((point, index) => (
+                                    <td><td contentEditable="true" style={{ width: "100px" }}>
+                                        {point.point}
+                                    </td>
+                                    <div style={{ display: 'inline'}}>/{point.pointGrade}</div>
                                     </td>
                                 ))}
                             </tr>
---
+++ src/component/detail-class/grade/ImportFile.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import { Form, Button, Modal } from 'react-bootstrap';
+
+function AddClassModal({ show, onHide, setFile }) {
+    
+    const [fileInput, setFileInput] = useState();
+    const [selectedFile, setSelectedFile] = useState(false);
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Import file csv
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <input
+                    type="file"
+                    accept=".csv"
+                    ref={(input) => setFileInput(input)} 
+                    onChange={()=> setSelectedFile(true)}
+                />
+            </Modal.Body>
+            <Modal.Footer> 
+                {selectedFile ? <Button className="btnAdd" type="submit" onClick={() =>{setFile(fileInput); setSelectedFile(false)}}>OK</Button>:<></>}
+                <Button className="btnAdd" onClick={() => {onHide(!show); setSelectedFile(false)}}>Cancel</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default AddClassModal;
