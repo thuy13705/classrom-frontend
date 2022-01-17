49cc4702a5d0cb3726daf6a97134d65604790bea
thuy13705 <18120587@student.hcmus.edu.vn>
2022-01-17 15:44:44 +0700

    remove panigation

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -13,9 +13,7 @@ function Home() {
     const history = useHistory();
     const [isCreate, setIsCreate] = useState(true);
     const [students, setStudent] = useState([]);
-    const [hiddenStudent, setHiddenStudent] = useState(true);
     const [teachers, setTeacher] = useState([]);
-    const [hiddenTeacher, setHiddenTeacher] = useState(true);
     const [modalShow, setModalShow] = useState(false);
 
     const handleModalShow = () => {
@@ -30,20 +28,9 @@ function Home() {
         }
         else if (result) {
             setStudent(result.students);
-            if (result.students.length > 3) {
-                setStudent(result.students.slice(0, 2));
-            }
-            if (result.students.length === 0) {
-                setHiddenStudent(false)
-            }
 
             setTeacher(result.teachers);
-            if (result.teachers.length > 3) {
-                setStudent(result.teachers.slice(0, 2));
-            }
-            if (result.teachers.length === 0) {
-                setHiddenTeacher(false)
-            }
+          
         }
     }
        
@@ -79,16 +66,6 @@ function Home() {
                     ))}
                 </Row>
 
-                {
-                    hiddenTeacher ? <div style={{ display: "flex", justifyContent: "end" }}>
-                        <NavLink style={{ textDecorationLine: "none", color: "#282c34" }} to={`/create`}>
-                            <Button>
-                                <FontAwesomeIcon icon={faAngleDoubleRight} />
-                            </Button>
-                        </NavLink>
-                    </div> : <></>
-                }
-
                 {/* class join */}
                 <h3 style={{ textAlign: "left", marginTop: "50px" }}>My Participatory Classes</h3>
 
@@ -102,16 +79,6 @@ function Home() {
                         </Col>
                     ))}
                 </Row>
-
-                {hiddenStudent ? <div style={{ display: "flex", justifyContent: "end" }}>
-                    <NavLink style={{ textDecorationLine: "none", color: "#282c34" }} to={`/join`}>
-                        <Button>
-                            <FontAwesomeIcon icon={faAngleDoubleRight} />
-                        </Button>
-                    </NavLink>
-
-                </div> : <></>
-                }
 
             </div>
 
